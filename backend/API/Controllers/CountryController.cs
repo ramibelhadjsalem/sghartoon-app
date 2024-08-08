76533a004ef899a2ea740378b7cef8a88d1ae2ea
ramibelhadjsalem <60022426+ramibelhadjsalem@users.noreply.github.com>
2024-08-08 02:53:49 +0100

    Update CountryController.cs

--- backend/API/Controllers/CountryController.cs
+++ backend/API/Controllers/CountryController.cs
@@ -42,7 +42,7 @@ namespace API.Controllers
         {
             if (country == null)
             {
-                return BadRequest("Specialite is null.5");
+                return BadRequest("Specialite is null.55");
             }
             await _repository.Create(country);
             return CreatedAtAction(nameof(GetById), new { id = country.Id }, country);
